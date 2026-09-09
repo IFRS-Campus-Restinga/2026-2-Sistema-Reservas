d96d508cd3002aadb83304c95173841b1cf88373
Guilherme Ramgrab Rodrigues <guilhermerodrigues433@gmail.com>
2026-09-09 00:40:23 -0300

    feat: Cria header conforme protótipo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,51 @@
-function Header() {
-  return (
-    <header className="header">
-      <h1>Meu Sistema</h1>
+import styles from "./Header.module.css";
+import { Bell, Plus } from "lucide-react";
 
-      <div className="header-actions">
-        <span>Usuário</span>
+const usuario = {
+  nome: "Carlos Mendes",
+  iniciais: "CM",
+  perfil: "Administrador",
+};
+
+function Header({ titulo }) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.infoHeader}>
+        <h2 className={styles.titulo}>{titulo}</h2>
+        <p className={styles.subTitulo}> Campus Restinga, IFRS </p>
+      </div>
+
+      <div className={styles.acoes}>
+        <div className={styles.relative}>
+          <button type="button" className={styles.botaNovaReserva}>
+            <Plus size={15} />
+            <span>Nova reserva</span>
+          </button>
+        </div>
+
+        <div className={styles.relative}>
+          <button
+            type="button"
+            className={styles.botaoNotificacao}
+            aria-label="Notificações"
+          >
+            <Bell size={18} />
+          </button>
+        </div>
+
+        <div className={styles.containerPerfil}>
+          <div className={styles.infoPerfil}>
+            <div className={styles.dadosUsuario}>
+              <p className={styles.nomeUsuario}>{usuario.nome}</p>
+              <p className={styles.perfilUsuario}>{usuario.perfil}</p>
+            </div>
+
+            <div className={styles.avatar}>{usuario.iniciais}</div>
+          </div>
+        </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
