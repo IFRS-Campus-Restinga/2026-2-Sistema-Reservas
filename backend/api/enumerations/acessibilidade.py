d765c9707af071383b117e53312351050ecf23df
Leo Andrighetto <leoandrighetto02@gmail.com>
2026-09-10 23:08:51 -0300

    feat/backend: atualização de enums de acessibilidade

--- backend/api/enumerations/acessibilidade.py
+++ backend/api/enumerations/acessibilidade.py
@@ -2,7 +2,5 @@ from django.db import models
 
 class Acessibilidade(models.TextChoices):
     PISO_TATIL = 'PISO_TATIL', 'Piso Tátil'
-    PORTAS_LARGAS = 'PORTAS_LARGAS', 'Portas Largas'
-    RAMPAS = 'RAMPAS', 'Rampas'
-    BANHEIRO_ADAPTADO = 'BANHEIRO_ADAPTADO', 'Banheiro Adaptado'
-    ELEVADOR = 'ELEVADOR', 'Elevador'
+    BANHEIRO = 'BANHEIRO', 'Banheiro Adaptado'
+    BEBEDOURO = 'BEBEDOURO', 'Bebedouro Adaptado'
