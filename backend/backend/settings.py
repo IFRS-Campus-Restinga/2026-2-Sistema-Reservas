44d2ca40d8543c842a5f1058ae9afe12a6630f18
Bia-Reichert <biamcd@gmail.com>
2026-09-13 00:58:27 -0300

    feat: renova access_token expirado via refresh_token no middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
+    'hub_integrations.middleware.RenovarTokenExpiradoMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +128,10 @@ FS_AUTH_SYSTEM_MODEL = 'hub_integrations.System'
 
 AUTH_COOKIE_NAME = env("AUTH_COOKIE_NAME", default="access_token")
 REFRESH_COOKIE_NAME = env("REFRESH_COOKIE_NAME", default="refresh_token")
+
+AUTH_COOKIE_HTTPONLY = env.bool("AUTH_COOKIE_HTTPONLY", default=True)
+AUTH_COOKIE_SECURE = env.bool("AUTH_COOKIE_SECURE", default=False)
+AUTH_COOKIE_SAMESITE = env("AUTH_COOKIE_SAMESITE", default="Lax")
 
 HUB_BASE_URL = env("HUB_BASE_URL", default="http://localhost:8000")
 HUB_SYSTEM_API_KEY = env("HUB_SYSTEM_API_KEY", default="")
