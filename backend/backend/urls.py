48bc3c584bb6f542c1fc1680257270d8ecf27b37
Bia-Reichert <biamcd@gmail.com>
2026-09-13 18:53:15 -0300

    feat: conecta o handoff de login do HUB à tela principal do React

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,8 @@ from django.urls import include, path
 from .views import ReactAppView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("session/", include("hub_integrations.urls")),
     path("", ReactAppView.as_view(), name="react"),
+    path("<path:rota>", ReactAppView.as_view(), name="react-catchall"),
 ]
