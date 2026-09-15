90f92d44c0cf9b3453f29fa1fbbc93efb512660f
Julia <juliarosa0618@gmail.com>
2026-09-15 16:19:42 -0300

    fix: ajustes na integracao com co back

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,7 @@ from .views import ReactAppView
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("session/", include("hub_integrations.urls")),
+    path("api/", include("api.urls")),
     path("", ReactAppView.as_view(), name="react"),
     path("<path:rota>", ReactAppView.as_view(), name="react-catchall"),
 ]
